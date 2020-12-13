81922d310552892eae455e46e7d232a984bf8563
zackkomo <zack.komo98@gmail.com>
2020-12-13 14:03:59 +0200

    changed homepage and nav workings

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,50 @@ import React from "react";
 import "./App.css";
 import Particles from "react-particles-js";
 import particlesConfig from "./particlesConfig.js";
+import hbg from "./assets/homebackground.jpg";
 function Home() {
   return (
     <div className="homepage">
-      <div style={{ position: "absolute", overflow: "hidden" }}>
-        <Particles height="100vh" width="100vw" params={particlesConfig} />
+      <div
+        style={{
+          overflow: "hidden",
+          maxHeight: "100%",
+          maxWidth: "100%",
+          zIndex: "0",
+        }}
+      >
+        <img
+          src={hbg}
+          alt="Snow"
+          style={{
+            position: "absolute",
+            width: "100vw",
+            height: "95vh",
+            resizeMode: "cover",
+          }}
+        />
       </div>
-      <div className="homepage-content">
-        <h1> Welcome to my website! Have a look around.</h1>
+      <div
+        style={{
+          position: "absolute",
+          overflow: "hidden",
+          maxHeight: "100%",
+          maxWidth: "100%",
+          zIndex: "1",
+        }}
+      >
+        <Particles height="94vh" width="100vw" params={particlesConfig} />
+      </div>
+      <div
+        style={{
+          position: "absolute",
+          textAlign: "center",
+          width: "100%",
+          top: "40vh",
+          zIndex: "2",
+        }}
+      >
+        <h1>Welcome tom my website! Have a look around.</h1>
       </div>
     </div>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -1,34 +1,52 @@
 import React from "react";
+
 import "./App.css";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-function Nav() {
+const Navitem = styled.nav`
+  height: 5vh;
+  width: 100%;
+  background: #313131;
+  color: white;
+  display: flex;
+`;
+const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  &:after {
+    color: red;
+  }
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    opacity: 0.5;
+    color: black;
+  }
+`;
+
+const Nav = () => {
   return (
-    <nav>
-      <ul className="nav-links">
-        <li className="Nav-link">
-          <Link to="/">
-            <h1>Home</h1>
-          </Link>
-        </li>
-        <li className="Nav-link">
-          <Link to="/about">
-            <h5>About</h5>
-          </Link>
-        </li>
-        <li className="Nav-link">
-          <Link to="/projects">
-            <h5> Projects</h5>
-          </Link>
-        </li>
-        <li className="Nav-link">
-          <Link to="/contact-me">
-            <h5>Contact Me</h5>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <Navitem>
+      <NavLink to="/" activeStyle>
+        <h1>Home</h1>
+      </NavLink>
+      <NavLink to="/about" activeStyle>
+        <h5>About</h5>
+      </NavLink>
+      <NavLink to="/projects" activeStyle>
+        <h5>Projects</h5>
+      </NavLink>
+      <NavLink to="/contact-me" activeStyle>
+        <h5>Contact Me</h5>
+      </NavLink>
+    </Navitem>
   );
-}
+};
 
 export default Nav;
