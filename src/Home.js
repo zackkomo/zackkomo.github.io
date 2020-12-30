2c3348d9c5cd3e85555b4305527962171109570c
zackkomo <zack.komo98@gmail.com>
2020-12-30 17:35:58 +0200

    changed homepage and background

--- src/Home.js
+++ src/Home.js
@@ -3,49 +3,18 @@ import "./App.css";
 import Particles from "react-particles-js";
 import particlesConfig from "./particlesConfig.js";
 import hbg from "./assets/homebackground.jpg";
+const backgroundImg = "./assets/homebackground.jpg";
 function Home() {
   return (
     <div className="homepage">
-      <div
-        style={{
-          overflow: "hidden",
-          maxHeight: "100%",
-          maxWidth: "100%",
-          zIndex: "0",
-        }}
-      >
-        <img
-          src={hbg}
-          alt="Snow"
-          style={{
-            position: "absolute",
-            width: "100vw",
-            height: "95vh",
-            resizeMode: "cover",
-          }}
-        />
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          overflow: "hidden",
-          maxHeight: "100%",
-          maxWidth: "100%",
-          zIndex: "1",
-        }}
-      >
+      <div className="particles">
         <Particles height="94vh" width="100vw" params={particlesConfig} />
       </div>
-      <div
-        style={{
-          position: "absolute",
-          textAlign: "center",
-          width: "100%",
-          top: "40vh",
-          zIndex: "2",
-        }}
-      >
-        <h1>Welcome tom my website! Have a look around.</h1>
+      <div className="homepageContent">
+        <div className="mybanner">
+          <h1>Zacharias Komodromos</h1>
+          <h3>Welcome to my website! Have a look around.</h3>
+        </div>
       </div>
     </div>
   );
